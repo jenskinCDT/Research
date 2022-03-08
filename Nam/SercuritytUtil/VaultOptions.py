035b5f170669c9a4a240380c553deb64d0891db2
jenskinCDT <97421119+jenskinCDT@users.noreply.github.com>
2022-03-08 10:30:49 +0700

    Add files via upload

---
+++ Nam/SercuritytUtil/VaultOptions.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, enabled, url, key, authType, token, username, password, securityKey):
+    self.enabled = enabled
+    self.url = url
+    self.key = key
+    self.authType = authType
+    self.token = token
+    self.username = username
+    self.password = password
+    self.securityKey = securityKey
