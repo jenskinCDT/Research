27b6fb357477104b256575a0a91cdcc0bda87fe2
xizot <nvnhat.17ck1@gmail.com>
2022-03-24 09:49:13 +0700

    update

--- Nhat/reactjs/src/pages/Home/Home.jsx
+++ Nhat/reactjs/src/pages/Home/Home.jsx
@@ -4,7 +4,38 @@ import TextField from '@mui/material/TextField';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 import DateAdapter from '@mui/lab/AdapterMoment';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import CustomTable from '../../components/CustomTable/CustomTable';
+import UserService from '../../services/UserService';
+import { toast } from 'react-toastify';
+const columns = [
+  { title: 'ID', field: 'id' },
+  { title: 'Title', field: 'title' },
+  { title: 'Name', field: 'name' },
+  {
+    title: 'Overview',
+    field: 'overview',
+    cellStyle: {
+      wordBreak: 'break-word',
+      maxWidth: 500,
+      minWidth: 500,
+    },
+  },
+  { title: 'Runtime', field: 'runtime' },
+  {
+    title: 'CreatedAt',
+    field: 'createdAt',
+    render: (rowData) => moment(rowData.createdAt).format('DD/MM/yyyy HH:mm:ss'),
+  },
 
+  {
+    title: 'ReleasedAt',
+    field: 'releasedAt',
+    render: (rowData) => moment(rowData.releasedAt).format('DD/MM/yyyy HH:mm:ss'),
+  },
+  { title: 'Status', field: 'status' },
+];
 function Home() {
   const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
 
@@ -14,7 +45,33 @@ function Home() {
 
   return (
     <Box padding={5} marginTop={5}>
-      <LocalizationProvider dateAdapter={DateAdapter}>
+      <CustomTable
+        title={'Posts'}
+        columns={columns}
+        data={(query) =>
+          new Promise((resolve, reject) => {
+            console.log(query);
+            let url = 'http://10.1.38.58:8000/items/?';
+            url += 'limit=' + query.pageSize;
+            url += '&page=' + (query.page + 1);
+            fetch(url, { headers: { Authorization: `Bearer ${UserService.getToken()}` } })
+              .then((response) => response.json())
+              .then((result) => {
+                resolve({
+                  data: result.data,
+                  page: result.page - 1,
+                  totalCount: result.totalCount || 0,
+                });
+              })
+              .catch((err) => {
+                reject(err);
+                console.log(err);
+                toast.error('Đã có lỗi xảy ra');
+              });
+          })
+        }
+      />
+      {/* <LocalizationProvider dateAdapter={DateAdapter}>
         <Box display={'flex'} marginBottom={2}>
           <DateTimePicker
             label="From Date"
@@ -34,7 +91,7 @@ function Home() {
             Search
           </Button>
         </Box>
-      </LocalizationProvider>
+      </LocalizationProvider> */}
     </Box>
   );
 }
--- Nhat/reactjs/src/services/UserService.js
+++ Nhat/reactjs/src/services/UserService.js
@@ -10,7 +10,8 @@ const _kc = new Keycloak('/keycloak.json');
 const initKeycloak = (onAuthenticatedCallback) => {
   _kc
     .init({
-      onLoad: 'check-sso',
+      onLoad: 'login-required',
+      // onLoad: 'check-sso',
       silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
       pkceMethod: 'S256',
     })
